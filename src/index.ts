827ac5c594506d694da0b16ad99cdf3f4b9661e9
Billy Fahey <billy@Billys-MacBook-Air.local>
2019-03-23 21:12:50 -0400

    Gradient upgrade!

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,87 @@
-type FillStyle = string | CanvasGradient | CanvasPattern
+type FillStyle = string | CanvasGradient | CanvasPattern;
 
 class Square {
-    x: number;
-    y: number;
-    length: number;
-  
-    constructor(x: number, y: number, length: number) {
-      this.x = x;
-      this.y = y;
-      this.length = length;
-    }
-  
-    draw(canvas: HTMLCanvasElement, color: FillStyle): void {
-      let c: CanvasRenderingContext2D = canvas.getContext("2d");
-      c.fillStyle = color;
-      c.fillRect(this.x, this.y, this.length, this.length);
-    }
+  x: number;
+  y: number;
+  length: number;
+
+  constructor(x: number, y: number, length: number) {
+    this.x = x;
+    this.y = y;
+    this.length = length;
   }
-  
-  function sierpinski(
-    canvas: HTMLCanvasElement,
-    x: number,
-    y: number,
-    length: number,
-    color: string,
-    depth: number
-  ): void {
-    let divisor = 4;
-    let my_gradient = canvas
-      .getContext("2d")
-      .createLinearGradient(x, y, x + length, y + length);
-    my_gradient.addColorStop(0, "purple");
-    my_gradient.addColorStop(0.125, "blue");
-    my_gradient.addColorStop(0.25, "green");
-    my_gradient.addColorStop(0.375, "yellow");
-    my_gradient.addColorStop(0.5, "orange");
-    my_gradient.addColorStop(0.625, "red");
-    my_gradient.addColorStop(0.75, "purple");
-    my_gradient.addColorStop(0.875, "blue");
-    my_gradient.addColorStop(1, "green");
-  
-    let s = new Square(x, y, length);
-    s.draw(canvas, my_gradient);
-    let delta = length / 3;
-    let center = new Square(x + delta, y + delta, delta);
-    center.draw(canvas, "white");
-    if (depth >= 1) {
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          if (i != 1 || j != 1) {
-            sierpinski(
-              canvas,
-              x + i * delta,
-              y + j * delta,
-              delta,
-              color,
-              depth - 1
-            );
-          }
+
+  draw(canvas: HTMLCanvasElement, color: FillStyle): void {
+    let c: CanvasRenderingContext2D = canvas.getContext("2d");
+    c.fillStyle = color;
+    c.fillRect(this.x, this.y, this.length, this.length);
+  }
+}
+
+function gradientMaker(canvas: HTMLCanvasElement, colors: string[]): CanvasGradient {
+  const ctx = canvas.getContext("2d");
+  const l = colors.length;
+  const g = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+  colors.map((c, idx) => {
+    g.addColorStop(idx * 1 / l, c);
+  });
+  return g;
+}
+
+function sierpinski(
+  canvas: HTMLCanvasElement,
+  x: number,
+  y: number,
+  length: number,
+  color: FillStyle,
+  depth: number
+): void {
+  let divisor = 4;
+  let my_gradient = canvas
+    .getContext("2d")
+    .createLinearGradient(x, y, x + length, y + length);
+  my_gradient.addColorStop(0, "purple");
+  my_gradient.addColorStop(0.125, "blue");
+  my_gradient.addColorStop(0.25, "green");
+  my_gradient.addColorStop(0.375, "yellow");
+  my_gradient.addColorStop(0.5, "orange");
+  my_gradient.addColorStop(0.625, "red");
+  my_gradient.addColorStop(0.75, "purple");
+  my_gradient.addColorStop(0.875, "blue");
+  my_gradient.addColorStop(1, "green");
+
+  let s = new Square(x, y, length);
+  s.draw(canvas, "black");
+  let delta = length / 3;
+  let center = new Square(x + delta, y + delta, delta);
+  center.draw(canvas, color);
+  if (depth >= 1) {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (i != 1 || j != 1) {
+          sierpinski(
+            canvas,
+            x + i * delta,
+            y + j * delta,
+            delta,
+            color,
+            depth - 1
+          );
         }
       }
     }
   }
-  
-  const canvas: HTMLCanvasElement = document.getElementById(
-    "ctx"
-  ) as HTMLCanvasElement;
-  
-  sierpinski(
-    canvas,
-    0,
-    0,
-    400,
-    "linear-gradient(to bottom, rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(32,124,202,1) 51%,rgba(125,185,232,1) 100%)",
-    2
-  );
-  
+}
+
+const canvas: HTMLCanvasElement = document.getElementById(
+  "ctx"
+) as HTMLCanvasElement;
+
+sierpinski(
+  canvas,
+  0,
+  0,
+  400,
+  gradientMaker(canvas, ["purple", "blue", "green", "yellow", "orange", "red"]),
+  4
+);
