e0d636c7b2b6aa02a5573868235adcbeb99db5df
Billy Fahey <billy@Billys-MacBook-Air.local>
2019-03-23 21:04:18 -0400

    rainbows!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+type FillStyle = string | CanvasGradient | CanvasPattern
+
 class Square {
     x: number;
     y: number;
@@ -9,7 +11,7 @@ class Square {
       this.length = length;
     }
   
-    draw(canvas: HTMLCanvasElement, color: string): void {
+    draw(canvas: HTMLCanvasElement, color: FillStyle): void {
       let c: CanvasRenderingContext2D = canvas.getContext("2d");
       c.fillStyle = color;
       c.fillRect(this.x, this.y, this.length, this.length);
@@ -24,8 +26,22 @@ class Square {
     color: string,
     depth: number
   ): void {
+    let divisor = 4;
+    let my_gradient = canvas
+      .getContext("2d")
+      .createLinearGradient(x, y, x + length, y + length);
+    my_gradient.addColorStop(0, "purple");
+    my_gradient.addColorStop(0.125, "blue");
+    my_gradient.addColorStop(0.25, "green");
+    my_gradient.addColorStop(0.375, "yellow");
+    my_gradient.addColorStop(0.5, "orange");
+    my_gradient.addColorStop(0.625, "red");
+    my_gradient.addColorStop(0.75, "purple");
+    my_gradient.addColorStop(0.875, "blue");
+    my_gradient.addColorStop(1, "green");
+  
     let s = new Square(x, y, length);
-    s.draw(canvas, color);
+    s.draw(canvas, my_gradient);
     let delta = length / 3;
     let center = new Square(x + delta, y + delta, delta);
     center.draw(canvas, "white");
@@ -51,5 +67,12 @@ class Square {
     "ctx"
   ) as HTMLCanvasElement;
   
-  sierpinski(canvas, 0, 0, 400, "black", 1);
+  sierpinski(
+    canvas,
+    0,
+    0,
+    400,
+    "linear-gradient(to bottom, rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(32,124,202,1) 51%,rgba(125,185,232,1) 100%)",
+    2
+  );
   
